edee2b87b2176864e878b1bf0dcdc37dfc0594cb
p12034156 <e12034156@student.tuwien.ac.at>
2024-03-22 18:36:35 +0100

    final code format with build succsess

--- backend/src/main/java/at/ac/tuwien/sepr/assignment/individual/rest/HorseEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepr/assignment/individual/rest/HorseEndpoint.java
@@ -26,63 +26,63 @@ import org.springframework.web.server.ResponseStatusException;
 @RestController
 @RequestMapping(path = HorseEndpoint.BASE_PATH)
 public class HorseEndpoint {
-    private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
-    static final String BASE_PATH = "/horses";
+  private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+  static final String BASE_PATH = "/horses";
 
-    private final HorseService service;
+  private final HorseService service;
 
-    public HorseEndpoint(HorseService service) {
-        this.service = service;
+  public HorseEndpoint(HorseService service) {
+    this.service = service;
+  }
+
+  @GetMapping
+  public Stream<HorseListDto> searchHorses(HorseSearchDto searchParameters) {
+    LOG.info("GET " + BASE_PATH);
+    LOG.debug("request parameters: {}", searchParameters);
+    return service.search(searchParameters);
+  }
+
+  @GetMapping("{id}")
+  public HorseDetailDto getById(@PathVariable("id") long id) {
+    LOG.info("GET " + BASE_PATH + "/{}", id);
+    try {
+      return service.getById(id);
+    } catch (NotFoundException e) {
+      HttpStatus status = HttpStatus.NOT_FOUND;
+      logClientError(status, "Horse to get details of not found", e);
+      throw new ResponseStatusException(status, e.getMessage(), e);
     }
+  }
 
-    @GetMapping
-    public Stream<HorseListDto> searchHorses(HorseSearchDto searchParameters) {
-        LOG.info("GET " + BASE_PATH);
-        LOG.debug("request parameters: {}", searchParameters);
-        return service.search(searchParameters);
+
+  @PutMapping("{id}")
+  public HorseDetailDto update(@PathVariable("id") long id, @RequestBody HorseDetailDto toUpdate) throws ValidationException, ConflictException {
+    LOG.info("PUT " + BASE_PATH + "/{}", toUpdate);
+    LOG.debug("Body of request:\n{}", toUpdate);
+    try {
+      return service.update(toUpdate.withId(id));
+    } catch (NotFoundException e) {
+      HttpStatus status = HttpStatus.NOT_FOUND;
+      logClientError(status, "Horse to update not found", e);
+      throw new ResponseStatusException(status, e.getMessage(), e);
     }
+  }
 
-    @GetMapping("{id}")
-    public HorseDetailDto getById(@PathVariable("id") long id) {
-        LOG.info("GET " + BASE_PATH + "/{}", id);
-        try {
-            return service.getById(id);
-        } catch (NotFoundException e) {
-            HttpStatus status = HttpStatus.NOT_FOUND;
-            logClientError(status, "Horse to get details of not found", e);
-            throw new ResponseStatusException(status, e.getMessage(), e);
-        }
+  @PostMapping("")
+  public HorseDetailDto createNewHorse(@RequestBody HorseDetailDto toCreate) throws ValidationException, ConflictException {
+    LOG.info("PUT " + BASE_PATH + "/{}", toCreate);
+    LOG.debug("Body of request:\n{}", toCreate);
+    try {
+      return service.createNewHorse((toCreate));
+    } catch (Exception e) {
+      HttpStatus status = HttpStatus.BAD_REQUEST;
+      logClientError(status, "The body contains invalid data", e);
+      throw new ResponseStatusException(status, e.getMessage(), e);
     }
+  }
 
 
-    @PutMapping("{id}")
-    public HorseDetailDto update(@PathVariable("id") long id, @RequestBody HorseDetailDto toUpdate) throws ValidationException, ConflictException {
-        LOG.info("PUT " + BASE_PATH + "/{}", toUpdate);
-        LOG.debug("Body of request:\n{}", toUpdate);
-        try {
-            return service.update(toUpdate.withId(id));
-        } catch (NotFoundException e) {
-            HttpStatus status = HttpStatus.NOT_FOUND;
-            logClientError(status, "Horse to update not found", e);
-            throw new ResponseStatusException(status, e.getMessage(), e);
-        }
-    }
-
-    @PostMapping("")
-    public HorseDetailDto createNewHorse(@RequestBody HorseDetailDto toCreate) throws ValidationException, ConflictException {
-        LOG.info("PUT " + BASE_PATH + "/{}", toCreate);
-        LOG.debug("Body of request:\n{}", toCreate);
-        try {
-            return service.createNewHorse((toCreate));
-        } catch (Exception e) {
-            HttpStatus status = HttpStatus.BAD_REQUEST;
-            logClientError(status, "The body contains invalid data", e);
-            throw new ResponseStatusException(status, e.getMessage(), e);
-        }
-    }
-
-
-    private void logClientError(HttpStatus status, String message, Exception e) {
-        LOG.warn("{} {}: {}: {}", status.value(), message, e.getClass().getSimpleName(), e.getMessage());
-    }
+  private void logClientError(HttpStatus status, String message, Exception e) {
+    LOG.warn("{} {}: {}: {}", status.value(), message, e.getClass().getSimpleName(), e.getMessage());
+  }
 }
